4c50a542a989e5b84666ca248eaee5457b1fc244
Moon <mooncactusmoon@gmail.com>
2022-02-11 08:33:27 +0800

    新增base

---
+++ base.php
@@ -0,0 +1,121 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    protected $dsn="mysql:hoset=localhost;charset=utf8;dbname=web04";
+    protected $pdo;
+    public $table;
+
+    public function __construct($table)
+    {
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+    //all
+    public function all(...$arg){
+        $sql="select * from $this->table ";
+        switch(count($arg)){
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key=>$value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .=" where ".implode(" AND ",$tmp);
+                }else{
+                    $sql .=$arg[0];
+                }
+            break;
+            case 2:
+                foreach($arg[0] as $key=>$value){
+                    $tmp[]="`$key`='$value'";
+                }
+                $sql .=" where ".implode(" AND ",$tmp)." ".$arg[1];
+            break;
+        }
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    //math
+    public function math($math,$col,...$arg){
+        $sql="select $math($col) from $this->table ";
+        switch(count($arg)){
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key=>$value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .=" where ".implode(" AND ",$tmp);
+                }else{
+                    $sql .=$arg[0];
+                }
+            break;
+            case 2:
+                foreach($arg[0] as $key=>$value){
+                    $tmp[]="`$key`='$value'";
+                }
+                $sql .=" where ".implode(" AND ",$tmp)." ".$arg[1];
+            break;
+        }
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    //find
+    public function find($id){
+        $sql="select * from $this->table where ";
+  
+            if(is_array($id)){
+                foreach($id as $key=>$value){
+                    $tmp[]="`$key`='$value'";
+                }
+                $sql .=" where ".implode(" AND ",$tmp);
+            }else{
+                $sql .=" `id`='$id'";
+            }
+
+        //echo $sql;
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    //del
+    public function del($id){
+        $sql="DELETE FROM $this->table where ";
+        if(is_array($id)){
+            foreach($id as $key=>$value){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql .=" where ".implode(" AND ",$tmp);
+        }else{
+            $sql .=" `id`='$id'";
+        }
+        //echo $sql;
+        return $this->pdo->exec($sql);
+
+    }
+    //save
+    public function save($array){
+        if(isset($array['id'])){
+            //update
+            foreach($array as $key=>$value){
+                if($key!='id'){
+
+                    $tmp[]="`$key`='$value'";
+                }
+            }
+            $sql="UPDATE $this->table SET ".implode(",",$tmp)." WHERE `id`='{$array['id']}'";
+        }else{
+            //insert
+            $sql="INSERT INTO $this->table (`".implode("`,`",array_keys($array))."`) VALUES('".implode("','",$array)."')";
+        }
+        return $this->pdo->exec($sql);
+    }
+    //q
+    public function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+function to($url){
+    header("location:".$url);
+}
+?>
