d2baf53ca72402e5e0e6bfa0e6e846f403ce1b23
Moon <mooncactusmoon@gmail.com>
2022-02-11 16:36:29 +0800

    完成編輯管理帳號

--- api/save_admin.php
+++ api/save_admin.php
@@ -3,3 +3,5 @@ include_once "../base.php";
 
 $_POST['pr']=serialize($_POST['pr']);
 $Admin->save($_POST);
+
+to("../back.php?do=admin"); //如果用api的方式不用這一行
--- back/edit_admin.php
+++ back/edit_admin.php
@@ -1,47 +1,53 @@
-<h1 class="ct">新增管理帳號</h1>
+<?php
+$row=$Admin->find($_GET['id']);
+$pr=unserialize($row['pr']);
+
+?>
+
+<h1 class="ct">修改管理帳號</h1>
 <form action="api/save_admin.php" method="post">
     <table class="all">
         <tr>
             <td class="tt ct">帳號</td>
             <td class="pp">
-                <input type="text" name="acc" id="acc">
+                <input type="text" name="acc" id="acc" value="<?=$row['acc'];?>">
             </td>
         </tr>
         <tr>
             <td class="tt ct">密碼</td>
             <td class="pp">
-                <input type="password" name="pw" id="pw">
+                <input type="password" name="pw" id="pw" value="<?=$row['pw'];?>">
+                <input type="hidden" name="id" id="id" value="<?=$row['id'];?>">
             </td>
         </tr>
         <tr>
             <td class="tt ct">權限</td>
             <td class="pp">
                 <div>
-                    <input type="checkbox" name="pr[]" value="1">
+                    <input type="checkbox" name="pr[]" value="1" <?=(in_array(1,$pr))?"checked":"";?> >
                     商品分類與管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" value="2">
+                    <input type="checkbox" name="pr[]" value="2" <?=(in_array(2,$pr))?"checked":"";?> >
                     訂單管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" value="3">
+                    <input type="checkbox" name="pr[]" value="3" <?=(in_array(3,$pr))?"checked":"";?> >
                     會員管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" value="4">
+                    <input type="checkbox" name="pr[]" value="4" <?=(in_array(4,$pr))?"checked":"";?> >
                     頁尾版權區管理
                 </div>
                 <div>
-                    <input type="checkbox" name="pr[]" value="5">
+                    <input type="checkbox" name="pr[]" value="5" <?=(in_array(5,$pr))?"checked":"";?> >
                     最新消息管理
                 </div>
             </td>
         </tr>
     </table>
     <div class="ct">
-        <input type="submit" value="新增">|
+        <input type="submit" value="修改">|
         <input type="reset" value="重置">
     </div>
 </form>
-
