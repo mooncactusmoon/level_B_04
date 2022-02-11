52322b9d702abf103e55f978d354a0d9eda47d83
Moon <mooncactusmoon@gmail.com>
2022-02-11 15:45:37 +0800

    建置後台管理員管理頁面

--- api/chk_pw.php
+++ api/chk_pw.php
@@ -4,10 +4,17 @@ $db=new DB($_POST['table']);
 $chk=$db->math('count','*',['acc'=>$_POST['acc'],'pw'=>$_POST['pw']]);
 if($chk){
    echo 1;
+   switch($_POST['table']){
+      case "admin":
+         $_SESSION['admin']=$_POST['acc'];
+         break;
+      case "member":
+         $_SESSION['member']=$_POST['acc'];
+      break;
+   }
 }else{
    echo 0;
 }
 
-// echo $Mem->math('count','*',['acc'=>$_POST['acc']]);
 
 ?>
