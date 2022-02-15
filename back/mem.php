36ede25463b9e858745b1c88b7877050fac8385e
Moon <mooncactusmoon@gmail.com>
2022-02-15 08:50:03 +0800

    完成會員編輯

--- back/mem.php
+++ back/mem.php
@@ -1,1 +1,25 @@
-會員管理
+<div class="ct">會員管理</div>
+<table class="all ct">
+   <tr class="tt">
+      <td>姓名</td>
+      <td>會員帳號</td>
+      <td>註冊日期</td>
+      <td>操作</td>
+   </tr>
+   <?php
+   $rows=$Mem->all();
+   foreach($rows as $row){
+   ?>
+   <tr class="pp">
+      <td><?=$row['name'];?></td>
+      <td><?=$row['acc'];?></td>
+      <td><?=$row['regdate'];?></td> <!--date("Y-m-d",strtotime($row['regdate']))-->
+      <td>
+         <button onclick="location.href='?do=edit_mem&id=<?=$row['id'];?>'">修改</button>
+         <button onclick="del('member',<?=$row['id'];?>)">刪除</button>
+      </td>
+   </tr>
+   <?php
+   }
+   ?>
+</table>
