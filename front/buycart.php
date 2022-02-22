f5432e1bc7bf6bbcaa17d9c0de9b69bc3d7c54a2
Moon <mooncactusmoon@gmail.com>
2022-02-22 09:10:09 +0800

    建立購物車機制

--- front/buycart.php
+++ front/buycart.php
@@ -1,8 +1,18 @@
 <?php
+if(isset($_GET['id']) && isset($_GET['qt'])){
+   //存成2維陣列，裡面的每個陣列=[商品(id)=>幾個(qt)]
+   $_SESSION['cart'][$_GET['id']]=$_GET['qt']; 
+
+}
 if(!isset($_SESSION['member'])){
    to("?do=login");
    exit();
 }
-echo "你要購買的商品id為".$_GET['id'];
-echo "數量為".$_GET['qt'];
+if(empty($_SESSION['cart'])){
+   echo "<h1 class='ct'>購物車中無商品</h1>";
+}else{
+
+   print_r($_SESSION['cart']);
+}
+
 ?>
